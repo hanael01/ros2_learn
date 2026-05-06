8393119163b303784719d759a590045139f1740e
hanael01 <hanaeldream@gmail.com>
2026-05-07 01:17:56 +0200

    ajout du publisher

--- src/mon_bras/mon_bras/noeud_bras.py
+++ src/mon_bras/mon_bras/noeud_bras.py
@@ -1,15 +1,34 @@
 import rclpy
 from rclpy.node import Node
-
+from std_msgs.msg import Float32MultiArray
 
 
 class BrasRobotique(Node):
 
     def __init__(self):
         super().__init__("bras_robotique")
+        
+        self.publisher_ = self.create_publisher(
+            Float32MultiArray,
+            '/positions_joints',
+            10
+        )
+
+
+         
+        self.positions = [1.0, 0.0, 3.0, 0.0, 5.0, 0.0]
+
+        self.timer = self.create_timer(1.0, self.publier_positions)
+
         self.get_logger().info("Noeud bras robotique démarré !")
 
 
+
+    def publier_positions(self):
+        msg = Float32MultiArray()
+        msg.data = self.positions
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Positions publiées : {self.positions}")
 
 
 
