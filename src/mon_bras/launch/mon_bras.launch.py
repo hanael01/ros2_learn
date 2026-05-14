9ee62aa1af0533aea2478c597ac77fb5bd894839
hanael01 <hanaeldream@gmail.com>
2026-05-14 22:05:37 +0200

    integration Gazebo - bras spawné

--- src/mon_bras/launch/mon_bras.launch.py
+++ src/mon_bras/launch/mon_bras.launch.py
@@ -25,7 +25,7 @@ from ament_index_python.packages import get_package_share_directory
 
     ])"""
 
-def generate_launch_description():
+"""def generate_launch_description():
 
     urdf_file = os.path.join(
         get_package_share_directory('mon_bras'),
@@ -55,6 +55,49 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
+            output='screen'
+        ),
+
+    ])"""
+
+
+def generate_launch_description():
+
+    urdf_file = os.path.join(
+        get_package_share_directory('mon_bras'),
+        'urdf',
+        'mon_bras.urdf'
+    )
+
+    with open(urdf_file, 'r') as f:
+        robot_description = f.read()
+
+    return LaunchDescription([
+
+        # Publie la description du robot
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{'robot_description': robot_description}]
+        ),
+        
+        Node(
+            package='ros_gz_sim',
+            executable='gzserver',
+            name='gazebo',
+            output='screen',
+            parameters=[{'world_sdf_file': '/opt/ros/jazzy/opt/gz_sim_vendor/share/gz/gz-sim8/worlds/empty.sdf'}]
+        ),
+        
+        # Spawn le robot dans Gazebo
+        Node(
+            package='ros_gz_sim',
+            executable='create',
+            name='spawn_robot',
+            arguments=['-name', 'mon_bras',
+                       '-topic', 'robot_description'],
             output='screen'
         ),
 
